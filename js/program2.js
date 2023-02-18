0e9ea03367f25aef872cec99b542924b77078c38
somratsam <somratsam2@gmail.com>
2023-02-17 19:14:54 -0800

    create pentagon calculator

--- js/program2.js
+++ js/program2.js
@@ -1,27 +1,20 @@
-
-// create a function for reuse code
-function getInputFieldValueById(inputFieldId) {
-    const inputElement = document.getElementById(inputFieldId);
-    const inputFieldValueString = inputElement.value;
-    inputFieldValue = parseFloat(inputFieldValueString);
-    inputElement.value = '';
-    return inputFieldValue
-}
+// step 1
 //   get event listener to the calculate button
 document.getElementById('btn-calculate-rect').addEventListener('click', function () {
+    // step 2
     // Get the values of the input fields
     const rectangleFirstElement = getInputFieldValueById('input-field-rect-first');
     const rectangleSecondElement = getInputFieldValueById('input-field-rect-second');
-
+// step 3
     // Check validation
     if (isNaN(rectangleFirstElement) || isNaN(rectangleSecondElement)) {
         alert('Please enter valid numbers');
         return;
     }
-
+// step 4
     // Calculate the total value
     const rectangleTotalValue = 0.5 * rectangleFirstElement * rectangleSecondElement;
-
+// step 5
     // Display the total value in the final element
     const rectangleFinalValue = document.getElementById('rectangle-text-field');
     rectangleFinalValue.innerText = rectangleTotalValue;
